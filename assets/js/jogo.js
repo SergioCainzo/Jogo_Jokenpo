f746d9bdb6c13ef4d4cff09751290bb8eed43205
Sergio Cainzo <97249973+SergioCainzo@users.noreply.github.com>
2022-10-03 10:41:25 -0300

    Add files via upload

--- assets/js/jogo.js
+++ assets/js/jogo.js
@@ -1,60 +1,135 @@
-//Variáveis
+// Variáveis do Jogo
 const pedra = document.querySelector('.pedra');
+const papel = document.querySelector('.papel');
+const tesoura = document.querySelector('.tesoura');
+
+// Exibição do resultado
+let textoJogador = document.querySelector('.span_jogador');
+let textoComputador = document.querySelector('.span_comp');
 
 
-// Variáveis Imagens
-const span_joga = document.querySelector('.span_jogador');
-const span_com = document.querySelector('.span_comp');
+// Placar do jogo
+let placarJogador = document.querySelector('.placar_jogador');
+let placarComp = document.querySelector('.placar_comp');
 
-// Variáveis do Placar
-const placarJogador = document.querySelector('.pontos_jogador');
-const placarComp = document.querySelector('.pontos_comp');
+// Variável da condição
+let jogador = 0;
+let jogador1 = 1;
+let jogador2 = 2;
 
+//Escolha do jogo do player
+pedra.addEventListener('click', jogar);
+papel.addEventListener('click', jogar);
+tesoura.addEventListener('click', jogar);
 
-// Lista dos itens
-const opcoes = new Array(
-    'Pedra',
-    'Papel',
-    'Tesoura'
-);
-
-
-
-// Lógica do jogo
-let computador = Math.floor(Math.random() * 2);
-console.log(`O computador colocou: ${opcoes[computador]}`);
-console.log(`O Jogador colocou: ${opcoes[jogador]}`);
 
 // Função do Jogo
 function jogar(){
-    
-    if(computador == 0){ // Pedra ( Computador )
-        if(jogador == 0){ // Pedra
-            console.log('Deu empate');
-        } else if( jogador == 1){ //Papel
-            console.log('Jogador Ganhou!');
-        } else if (jogador == 2){ // Tesoura
-            console.log('Computador Ganhou!');
+    // Lista dos itens
+    const opcoes = new Array(
+        'Pedra',
+        'Papel',
+        'Tesoura'
+    );
+
+    // Lógica do jogo
+    const computador = Math.floor(Math.random() * 3);
+
+    if(jogador == 0) { // Pedra
+        if (computador == 0){ // Pedra
+            
+            textoJogador.textContent = 'Deu Empate';
+            textoJogador.style.color = 'brown';
+
+
+            textoComputador.textContent = 'Deu Empate';
+            textoComputador.style.color = 'brown';
+
+
+        }
+        else if(computador == 1){ // Papel
+            
+            textoJogador.textContent = 'Ganhou !!';
+            textoJogador.style.color = 'green';
+            
+            
+            textoComputador.textContent = 'Perdeu !!';
+            textoComputador.style.color = 'red';
+
+           
+        }
+        else { // Tesoura
+            
+            textoJogador.textContent = 'Perdeu !!';
+            textoJogador.style.color = 'red';
+
+            textoComputador.textContent = 'Ganhou !!';
+            textoComputador.style.color = 'green';
+
+
         }
     }
 
-    if(computador == 1){ // Papel ( Computador )
-        if(jogador == 0){ // Pedra
-            console.log('Computador Ganhou!');
-        } else if (jogador == 1){ // Papel
-            console.log('Deu empate');
-        } else if(jogador == 2){ // Tesoura
-            console.log('Jogador Ganhou!');
+    if(jogador1 == 1) { // Papel
+
+        if (computador == 0){ // Pedra
+
+            textoJogador.textContent = 'Venceu !!';
+            textoJogador.style.color = 'green';
+
+
+            textoComputador.textContent = 'Perdeu !!';
+            textoComputador.style.color = 'red';
+        }
+        else if(computador == 1){ // Papel
+            
+            textoJogador.textContent = 'Deu Empate';
+            textoJogador.style.color = 'brown';
+
+
+            textoComputador.textContent = 'Deu Empate';
+            textoComputador.style.color = 'brown';
+        }
+        else { // Tesoura
+            
+            textoJogador.textContent = 'Perdeu !!';
+            textoJogador.style.color = 'red';
+
+
+            textoComputador.textContent = 'Venceu !!';
+            textoComputador.style.color = 'green';
         }
     }
 
-    if(computador == 2){ // Tesoura ( Computador )
-        if(jogador == 0){ // Pedra
-            console.log('Jogador Ganhou!');
-        } else if(jogador == 1){ // Papel
-            console.log('Computador Ganhou!');
-        } else if(jogador == 2){ // Tesoura
-            console.log('Deu empate')
+    if(jogador2 == 2) { // Tesoura
+        if (computador == 0){ // Pedra
+            
+            textoJogador.textContent = 'Perdeu !!';
+            textoJogador.style.color = 'red';
+
+
+            textoComputador.textContent = 'Venceu !!';
+            textoComputador.style.color = 'green';
+        }
+        else if(computador == 1){ // Papel
+            
+            
+            textoJogador.textContent = 'Ganhou !!';
+            textoJogador.style.color = 'green';
+
+
+            textoComputador.textContent = 'Perdeu !!';
+            textoComputador.style.color = 'red';
+        }
+        else { // Tesoura
+            
+            
+            textoJogador.textContent = 'Deu Empate';
+            textoJogador.style.color = 'brown';
+
+
+            textoComputador.textContent = 'Deu Empate';
+            textoComputador.style.color = 'brown';
         }
     }
 
